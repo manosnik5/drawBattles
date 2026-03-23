c5d36c250a72b3b7308f6cf8c0633f786639da2f
Manos <manosnikitakis3@gmail.com>
2026-03-23 11:06:49 +0200

    add prisma config

---
+++ backend/src/lib/prisma.js
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set')
+}
+
+const globalForPrisma = globalThis
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
