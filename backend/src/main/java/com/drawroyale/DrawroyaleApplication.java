006d93e54fa94ca37886130db493f12a2d10167e
Manos <manosnikitakis3@gmail.com>
2026-04-19 14:20:07 +0300

    migrate backend from Node.js/Express to Spring Boot

---
+++ backend/src/main/java/com/drawroyale/DrawroyaleApplication.java
@@ -0,0 +1,13 @@
+package com.drawroyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawroyaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawroyaleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/drawroyale/DrawroyaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawroyale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawroyaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
