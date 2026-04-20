1c2316971c8cde3af107780fec146c734ae91315
Manos <manosnikitakis3@gmail.com>
2026-04-20 13:36:16 +0300

    add SPA fallback controller for React routing

---
+++ backend/src/main/java/com/drawroyale/controllers/SpaController.java
@@ -0,0 +1,13 @@
+package com.drawroyale.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+
+    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
